dfc6b1209eebfa1c72847b3977a3fc03683c1ae6
CaueFurui <cauefurui@gmail.com>
2020-09-02 18:20:21 -0300

    [DASHBOARD]Adding styles

--- src/pages/Dashboard/Dashboard.tsx
+++ src/pages/Dashboard/Dashboard.tsx
@@ -1,8 +1,73 @@
 import React from 'react';
-import { Title } from './styles';
+import { FiChevronRight } from 'react-icons/fi';
+import { Title, Form, Repositories } from './styles';
+import logo from '../../assets/logo.svg';
 
 const Dashboard: React.FC = () => {
-  return <Title>Dashboard</Title>;
+  return (
+    <>
+      <img src={logo} alt="Github explorer" />
+      <Title>Explore repositórios no Github</Title>
+
+      <Form>
+        <input placeholder="Digite o nome do repositório" />
+        <button type="submit">Pesquisar</button>
+      </Form>
+
+      <Repositories>
+        <a href="teste">
+          <img
+            src="https://avatars3.githubusercontent.com/u/48840933?s=460&u=398aa25c197720153fd9bc152fa16f3f416cb853&v=4"
+            alt="Caue Furui"
+          />
+          <div>
+            <strong>cauefurui/teste</strong>
+            <p>Teste bla bla bla</p>
+          </div>
+
+          <FiChevronRight size={20} />
+        </a>
+        <a href="teste">
+          <img
+            src="https://avatars3.githubusercontent.com/u/48840933?s=460&u=398aa25c197720153fd9bc152fa16f3f416cb853&v=4"
+            alt="Caue Furui"
+          />
+          <div>
+            <strong>cauefurui/teste</strong>
+            <p>Teste bla bla bla</p>
+          </div>
+
+          <FiChevronRight size={20} />
+        </a>
+
+        <a href="teste">
+          <img
+            src="https://avatars3.githubusercontent.com/u/48840933?s=460&u=398aa25c197720153fd9bc152fa16f3f416cb853&v=4"
+            alt="Caue Furui"
+          />
+          <div>
+            <strong>cauefurui/teste</strong>
+            <p>Teste bla bla bla</p>
+          </div>
+
+          <FiChevronRight size={20} />
+        </a>
+
+        <a href="teste">
+          <img
+            src="https://avatars3.githubusercontent.com/u/48840933?s=460&u=398aa25c197720153fd9bc152fa16f3f416cb853&v=4"
+            alt="Caue Furui"
+          />
+          <div>
+            <strong>cauefurui/teste</strong>
+            <p>Teste bla bla bla</p>
+          </div>
+
+          <FiChevronRight size={20} />
+        </a>
+      </Repositories>
+    </>
+  );
 };
 
 export default Dashboard;
